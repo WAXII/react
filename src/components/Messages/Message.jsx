ff07a4114fdcc6ee941da113ee3935fe953a5b93
Waxii <waxii.xxx@yandex.ru>
2021-02-24 12:05:07 +0300

    Lesson2

--- src/components/Messages/Message.jsx
+++ src/components/Messages/Message.jsx
@@ -1,5 +1,13 @@
-const Message = (props) => {
-    return <div className='message'>{props.text}</div>
-};
+import React from 'react';
+
+class Message extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div className='message'><b>{this.props.source}: </b>{this.props.text}</div>
+    }
+}
 
 export {Message};
--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,13 +1,18 @@
+import React from 'react';
 import {Message} from './';
 
-const Messages = (props) => {
-    const {list = []} = props;
-
-    return <div className='messages'>
-        {list.map((item,idx) => (
-            <Message text={item} key={idx} />
-        ))}
-    </div>
-};
+class Messages extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const {list = []} = this.props;
+        return <div className='messages'>
+            {list.map((item,idx) => (
+                <Message source={item.source} text={item.msg} key={idx} />
+            ))}
+        </div>
+    }
+}
 
 export {Messages};
