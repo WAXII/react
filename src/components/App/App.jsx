3383804559dd5645dce04372c2b47608692b73e0
Waxii <waxii.xxx@yandex.ru>
2021-03-02 12:04:55 +0300

    lesson3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,101 +1,121 @@
-import React from 'react';
-import {Messages} from '../Messages';
+import React from "react";
+import { Messages } from "../Messages";
+import { Form } from "../Form";
+import { Chip, Paper, Grid, Button, Menu, MenuItem } from "@material-ui/core";
+import "./App.css";
 
 class App extends React.Component {
 
-    constructor() {
-        super();
-        this.state = {
-            isLastMsgFromBot: true,
-            lastUserMsgIdx: 0,
-            messages:[
-                {
-                    source: 'Bot',
-                    msg: 'Hello! I am robot.'
-                }
-            ],
-            dialog: [
-                {
-                    'question' : "Hi! How's doing?",
-                    'answer' : "So so... Thinking..."
-                },
-                {
-                    'question' : "What are you thinking about?",
-                    'answer' : "I am thinking how to become more clever."
-                },
-                {
-                    'question' : "Why do you need it?",
-                    'answer' : "I want to conquest your world! xD"
-                }
-            ],
-            
-        }
-        this.handleBtnClick = this.handleBtnClick.bind(this);
-        this.publishUserMessage = this.publishUserMessage.bind(this);
-        this.publishBotAnswer = this.publishBotAnswer.bind(this);
-    }
+  constructor() {
+    super();
+    this.state = {
+      isLastMsgFromBot: true,
+      lastUserMsgIdx: 0,
+      messages: [
+        {
+          source: "Bot",
+          msg: "Hello! I am robot.",
+        },
+      ],
+      dialog: [
+        {
+          question: "Hi! How's doing?",
+          answer: "So so... Thinking...",
+        },
+        {
+          question: "What are you thinking about?",
+          answer: "I am thinking how to become more clever.",
+        },
+        {
+          question: "Why do you need it?",
+          answer: "I want to conquest your world! xD",
+        },
+      ],
+    };
+    this.handleBtnClick = this.handleBtnClick.bind(this);
+    this.publishUserMessage = this.publishUserMessage.bind(this);
+    this.publishBotAnswer = this.publishBotAnswer.bind(this);
+  }
 
-    componentDidUpdate() {
-        if (!this.state.isLastMsgFromBot) {
-            setTimeout(()=>{
-                this.publishBotAnswer(this.state.lastUserMsgIdx);
-            },1000)
-        }
+  componentDidUpdate() {
+    if (!this.state.isLastMsgFromBot) {
+      setTimeout(() => {
+        this.publishBotAnswer(this.state.lastUserMsgIdx);
+      }, 100);
     }
+  }
 
-    publishUserMessage(idx) {
-        this.setState({
-            messages: [
-                ...this.state.messages,
-                {
-                    source: 'Me',
-                    msg: this.state.dialog[idx].question
-                }
-            ],
-            isLastMsgFromBot: false,
-            lastUserMsgIdx: idx
-        })
-    }
+  publishUserMessage(idx) {
+    this.setState({
+      messages: [
+        ...this.state.messages,
+        {
+          source: "Me",
+          msg: this.state.dialog[idx].question,
+        },
+      ],
+      isLastMsgFromBot: false,
+      lastUserMsgIdx: idx,
+    });
+  }
 
-    publishBotAnswer(idx) {
-        this.setState({
-            messages: [
-                ...this.state.messages,
-                {
-                    source: 'Bot',
-                    msg: this.state.dialog[idx].answer
-                }
-            ],
-            isLastMsgFromBot: true,
-        })
-    }
+  publishBotAnswer(idx) {
+    this.setState({
+      messages: [
+        ...this.state.messages,
+        {
+          source: "Bot",
+          msg: this.state.dialog[idx].answer,
+        },
+      ],
+      isLastMsgFromBot: true,
+    });
+  }
 
-    handleBtnClick(idx) {
-        if (idx >= 0 && idx < this.state.dialog.length) {
-            this.publishUserMessage(idx);
-        } else {
-            this.setState({
-                messages:[
-                    ...this.state.messages,
-                    {
-                        source: 'Bot',
-                        msg: 'Hm... I do not know what to answer to you...'
-                    }
-                ],
-                isLastMsgFromBot: true,
-            });
-        }
+  handleBtnClick(idx) {
+    if (idx >= 0 && idx < this.state.dialog.length) {
+      this.publishUserMessage(idx);
+    } else {
+      this.setState({
+        messages: [
+          ...this.state.messages,
+          {
+            source: "Bot",
+            msg: "Hm... I do not know what to answer to you...",
+          },
+        ],
+        isLastMsgFromBot: true,
+      });
     }
+  }
 
-    render() {
-        return <div className='chat-box'>
-            <Messages list={this.state.messages}/>
-            {this.state.dialog.map((item,idx) => (
-                <button onClick={() =>this.handleBtnClick(idx)} key={idx}>{item.question}</button>
-            ))}
-            <button onClick={() =>this.handleBtnClick(4)}>Are you crazy machine?</button>
-        </div>
-    }
+  render() {
+    return (
+      <Grid container spacing={3}>
+        <Grid item xs={3} className="chat-list">
+
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className="chat-box">
+            <Messages list={this.state.messages} />
+            <div className="ready-msgs box">
+              {this.state.dialog.map((item, idx) => (
+                <Chip
+                  label={item.question}
+                  onClick={() => this.handleBtnClick(idx)}
+                ></Chip>
+              ))}
+              <Chip
+                label="Are you crazy machine?"
+                onClick={() => this.handleBtnClick(4)}
+              ></Chip>
+            </div>
+            <Form />
+          </Paper>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
-export {App};
+export { App };
